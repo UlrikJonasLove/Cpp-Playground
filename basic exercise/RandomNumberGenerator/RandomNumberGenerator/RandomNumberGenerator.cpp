79b31facdee2f970922572868533355944f47c20
Jonas Rosberg <rojo19@student.ju.se>
2020-11-15 10:01:03 +0100

    update for random number generator

--- basic exercise/RandomNumberGenerator/RandomNumberGenerator/RandomNumberGenerator.cpp
+++ basic exercise/RandomNumberGenerator/RandomNumberGenerator/RandomNumberGenerator.cpp
@@ -7,13 +7,33 @@ using namespace std;
 int main()
 {
     srand(time(0));
+
     int randNum = rand() % 36 + 1;
-    // std::cout << randNum << std::endl;
+    int playerSum = 1000; 
+    int numInput;
+
+    string yourCoice;
+    string textInput;
+
+    cout << "Welcome to the roulette game!\nYour coice - Number or color:  ";
+
+    cin >> numInput;
+    cin >> textInput;
+
+    if (textInput == "number" || "Number" || "Color" || "color") 
+    {
+        cout << "This is some text ";
+    }
 
     if (randNum % 2 == 0)
     {
         system("color F0");
         cout << randNum;
+
+        if (numInput % 2 == 0 || textInput == "blue")
+        {
+
+        }
     }
 
     else
